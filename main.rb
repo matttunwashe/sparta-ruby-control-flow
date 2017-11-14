5786a7a766d1bd2b877ced2ce426a9eb7fb899b2
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-11-14 15:33:15 +0000

    control flow completed

---
+++ main.rb
@@ -0,0 +1,54 @@
+# conditional = 11
+#
+# if conditional < 10
+#     puts 'hello'
+# end
+#
+# if conditional < 10
+#     puts 'hello'
+# end
+#
+# if conditional < 10
+#     puts 'hello'
+# elseif conditional < 4
+#     puts 'thing'
+# else
+#     puts "something else"
+# end
+
+val = 1
+# if(val == 1) then puts "1" else "Not 1" end
+#or
+# puts if(val == 1)
+#  "1"
+# else "Not 1"
+# end
+
+puts val < 10 ? "it was true" : "it was false"
+# ==
+# 1=
+# >
+# <
+# >=
+# <=
+# <=>
+# .eql?
+# equal?
+
+
+#logical operators
+# and
+# or
+# &&
+# Not
+# ||
+# !
+
+
+
+
+
+#LOOPSs
+#Do while
+i = 11;
+begin 
