0a9cf1a3e7fb88a372df8dd4b7850b6562979219
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-11-15 12:27:06 +0000

    examples done

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@
 #     puts "something else"
 # end
 
-val = 1
+# val = 1
 # if(val == 1) then puts "1" else "Not 1" end
 #or
 # puts if(val == 1)
@@ -24,7 +24,7 @@ val = 1
 # else "Not 1"
 # end
 
-puts val < 10 ? "it was true" : "it was false"
+# puts val < 10 ? "it was true" : "it was false"
 # ==
 # 1=
 # >
@@ -50,5 +50,50 @@ puts val < 10 ? "it was true" : "it was false"
 
 #LOOPSs
 #Do while
-i = 11;
-begin 
+# i = 10
+# while i < 10 do
+#     puts "While: #{i}"
+#     i+=1
+# end
+
+
+# i = 11
+# begin
+#     puts "Do while: #{i}"
+#     i+=1
+# end while i < 10
+
+#until
+# i = 0
+# until i > 10 do
+#     puts "Until: #{i}"
+#     i+=1
+# end
+
+#FOR LOOPSs
+# for i in 0...5
+#     puts "For loop are great: #{i}"
+# end
+
+#each LOOPSs
+# (0..5).each do |i|
+#     puts "value of local variable is #{i}"
+# end
+
+more_words = ["one", "two", "three"]
+# reveresed_words = more_words.map do |word|
+#     word.reverse
+# end
+# puts reveresed_words
+
+# more_words.map! do |word|
+#     word.reverse
+# end
+#
+# puts more_words
+
+some_numbers = [1,4,5,2,3,1,2,4]
+sum = some_numbers.reduce 0 do |total, number|
+    total + number
+end
+puts sum
